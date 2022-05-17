7f4290b746358bd9705c76b4307d439ced313274
John Cuevas <jpmcuevas20@gmail.com>
2022-05-17 13:22:38 -0700

    one away

--- Cracking Interview/oneAway.js
+++ Cracking Interview/oneAway.js
@@ -1,26 +1,24 @@
-// not working solution
-
 function oneEditAway(first, second) {
     // Length check
-    if (Math.abs(first.length() - second.length()) > 1) {
+    if (Math.abs(first.length - second.length) > 1) {
         return false;
     }
 
     // Get shorter and longer string
-    let s1 = first.length() < second.length() ? first : second;
-    let s2 = first.length() < second.length() ? second : first;
+    let s1 = first.length < second.length ? first : second;
+    let s2 = first.length < second.length ? second : first;
 
     let index1 = 0;
     let index2 = 0;
     let foundDifference = false;
 
-    while (index2 < s2.length() && index1 < s1.length()) {
+    while (index2 < s2.length && index1 < s1.length) {
         if (s1.charAt(index1) != s2.charAt(index2)) {
             // Ensure that this is the first differene found
             if (foundDifference) return false;
             foundDifference = true;
 
-            if (s1.length() == s2.length()) {
+            if (s1.length == s2.length) {
                 index1++;
             }
         }
@@ -33,6 +31,6 @@ function oneEditAway(first, second) {
 }
 
 let first = "pale";
-let second = "ple";
+let second = "bake";
 
-oneEditAway(first, second);
+console.log(oneEditAway(first, second));
