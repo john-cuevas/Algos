6c5aea5bf6c1935287076f70a6ef20f2117138e6
John Cuevas <jpmcuevas20@gmail.com>
2022-05-27 09:39:39 -0700

    Replace with alphabet position

---
+++ Cracking Interview/replaceWithAlphabetPosition.js
@@ -0,0 +1,14 @@
+function alphabetPosition(text) {
+    let test = "abcdefghijklmnopqrstuvwxyz";
+    text = text.toLowerCase();
+    let textArr = [];
+    for (let i = 0; i < text.length; i++) {
+        for (let j = 0; j < test.length; j++) {
+            if (text[i] === test[j]) {
+                textArr.push(j + 1);
+            }
+        }
+    }
+    text = textArr.join(" ");
+    return text;
+}
