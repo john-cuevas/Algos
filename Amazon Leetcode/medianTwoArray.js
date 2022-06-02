e64a7967c0125fbd413db584bef71638fd7340ab
John Cuevas <jpmcuevas20@gmail.com>
2022-06-02 11:49:26 -0700

    get median of two arrays

---
+++ Amazon Leetcode/medianTwoArray.js
@@ -0,0 +1,34 @@
+// Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+function median(nums1, nums2){
+    // add the two arrays together
+    let joinedArray = nums1.concat(nums2);
+
+    // sort array ascending order
+    joinedArray.sort((a,b) => a-b);
+    console.log(joinedArray);
+
+    let half = Math.floor(joinedArray.length / 2);
+    console.log(half);
+    console.log(joinedArray[half]);
+
+    if(joinedArray.length %2 == true){
+        return joinedArray[half];
+    }
+    else{
+        return (joinedArray[half] + joinedArray[half-1])/2
+    }
+    
+}
+
+let nums1 = [1,2];
+let nums2 = [3,4];
+
+console.log(median(nums1, nums2));
+
+// var half = Math.floor(values.length / 2);
+  
+// if (values.length % 2)
+//   return values[half];
+
+// return (values[half - 1] + values[half]) / 2.0;
