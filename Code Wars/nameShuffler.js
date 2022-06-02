14eb177f55f33b2ae9256c5dd60d16e480063ccd
John Cuevas <jpmcuevas20@gmail.com>
2022-06-02 16:26:04 -0700

    name shuffler

---
+++ Code Wars/nameShuffler.js
@@ -0,0 +1,26 @@
+function nameShuffler(str) {
+    // let newStr = str.split(" ");
+
+    // Don't even need swap
+
+    //   let temp = newStr[0];
+    //   newStr[0] = newStr[1];
+    //   newStr[1] = temp;
+
+    // [newStr[0], newStr[1]] = [newStr[1], newStr[0]];
+
+    // let result = newStr[1] + " " + newStr[0];
+
+    // return result;
+
+    // One line solution
+    // split turns string into an array
+    // reverse swaps order of the array
+    // join combines the elements of the array into a string
+
+    return str.split(" ").reverse().join(" ");
+    // return str.split(" ").reverse().join(" ");
+}
+
+let str1 = "John Cuevas";
+console.log(nameShuffler(str1));
