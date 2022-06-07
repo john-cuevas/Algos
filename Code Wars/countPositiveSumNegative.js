aaca695a6e78fd785cc96068e6f5b6fed76d6feb
John Cuevas <jpmcuevas20@gmail.com>
2022-06-06 18:38:58 -0700

    cout positive some negatives of array

--- Code Wars/countPositiveSumNegative.js
+++ Code Wars/countPositiveSumNegative.js
@@ -1,12 +1,17 @@
 function countPositivesSumNegatives(input) {
     let positiveSum = 0;
     let negativeSum = 0;
-    if (input.length == 0) {
-        return 0;
+    // did this to account for zeros in array. 0 doesn't count as positive or negative
+    let zeros = 0;
+    if (!input || input.length == 0) {
+        return [];
     }
     for (let i = 0; i < input.length; i++) {
         if (input[i] > 0) {
             positiveSum++;
+        }
+        else if (input[i] == 0) {
+            zeros++;
         }
         else {
             negativeSum += input[i];
