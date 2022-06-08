32dc60c4091c3d5ea74bbdb64e2a4f614f34e5c2
John Cuevas <jpmcuevas20@gmail.com>
2022-06-08 15:15:53 -0700

    sum two smallest positive numbers in array

--- Code Wars/sumTwoSmallestNumbers.js
+++ Code Wars/sumTwoSmallestNumbers.js
@@ -1,6 +1,6 @@
 function sumTwoSmallestNumbers(numbers) {
     let result = [];
-
+    
     // makes sure result array is only positive numbers
     for (let i = 0; i < numbers.length; i++) {
         if(numbers[i] > 0){
@@ -9,8 +9,9 @@ function sumTwoSmallestNumbers(numbers) {
     }
     // sort array
     result.sort((a,b) => a - b);
+    
+    // show result array
     console.log(result);
-
     return result[0] + result[1];
 }
 
