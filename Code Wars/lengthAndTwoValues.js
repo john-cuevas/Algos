7f132801423462af644bf8d2246085dd8f778d2b
John Cuevas <jpmcuevas20@gmail.com>
2022-06-14 22:31:01 -0700

    alternate array with n length, two inputted values

---
+++ Code Wars/lengthAndTwoValues.js
@@ -0,0 +1,34 @@
+function alternate(n, firstValue, secondValue) {
+    let result = [];
+
+    // base case if n = 1
+    if (n == 1){
+        result.push(firstValue);
+    }
+
+    for (let i = 0; i < n; i++) {
+        // if (result[i] == null) {
+        //     result.push(firstValue);
+        // }
+        // if(result[i] == firstValue){
+        //     result.push(secondValue);
+        // }
+        // else{
+        //     result.push(firstValue);
+        // }
+
+        if( i %2 == 0){
+            result.push(firstValue);
+        }
+        else{
+            result.push(secondValue);
+        }
+    }
+    return result;
+}
+
+let n = 5;
+let firstValue = "true";
+let secondValue = "false";
+
+console.log(alternate(n, firstValue, secondValue));
