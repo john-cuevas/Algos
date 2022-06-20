3af30dd7467e3fcbbeaf194a12b12a742df4648e
John Cuevas <jpmcuevas20@gmail.com>
2022-06-20 11:49:50 -0700

    shorter solution check if square a whole number

--- Code Wars/youreASquare.js
+++ Code Wars/youreASquare.js
@@ -1,13 +1,19 @@
 function isSquare (num){
-    if(num < 0){
-        return false;
-    }
+    // if(num < 0){
+    //     return false;
+    // }
+    // if(Math.sqrt(num) %1 == 0){
+    //     return true
+    // }
+    // else if(Math.sqrt(num) %1 !=0){
+    //     return false;
+    // }
+
+    // better version
     if(Math.sqrt(num) %1 == 0){
-        return true
+        return true;
     }
-    else if(Math.sqrt(num) %1 !=0){
-        return false;
-    }
+    return false;
 }
 let num1 = 3;
 console.log(isSquare(num1));
