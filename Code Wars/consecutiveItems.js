4681acb2ba9d09024710e993941c794aea292016
John Cuevas <jpmcuevas20@gmail.com>
2022-06-24 11:29:11 -0700

    check if array has consecutive items

---
+++ Code Wars/consecutiveItems.js
@@ -0,0 +1,21 @@
+const consecutive = (arr, a, b) => {
+
+    // My solution
+    let count = 0;
+    for(let i = 0; i < arr.length; i++){
+        if((arr[i] == a && arr[i+1] == b) || (arr[i] == b && arr[i+1] == a)){
+            count++;
+        }
+    }
+    if(count ==1){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+let array1 = [ 1, 3, 5, 7];
+let a = 1;
+let b = 3;
+console.log(consecutive(array1, a, b));
