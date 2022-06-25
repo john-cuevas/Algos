4c0a3a3b51f779ef05b9af0103454fd5bbde0fcd
John Cuevas <jpmcuevas20@gmail.com>
2022-06-25 15:25:07 -0700

    correct solution possibilities array

--- Code Wars/possibilitiesArray.js
+++ Code Wars/possibilitiesArray.js
@@ -2,18 +2,38 @@
 
 const isAllPossibilities = (x) =>{
     let count =0;
-    let length = x.length -1;
-    for(let i =0; i< x.length; i++){
-        if(x[i]< 0 || x[i] >= length){
-            count++;
+    // let length = x.length -1;
+    // My solution - not correct
+    // if(x.length ==0){
+    //     return false;
+    // }
+    // for(let i =0; i< x.length; i++){
+    //     if(x[i]< 0 || x[i] >= length){
+    //         count++;
+    //     }
+    // }
+    // console.log([0, length]);
+    // console.log(count);
+    // if(count == 0){
+    //     return true;
+    // }
+    // return false;
+
+    // correct solution
+    if(x.length >=1){
+        for(let i =0; i < x.length; i++){
+            if(x.includes(i)){
+                count++;
+            }
         }
+        return count === x.length;
     }
-    if(count == 0){
-        return true;
+    else{
+        return false;
     }
-    return false;
+    
 
 }
 
-let arr1 = [1, 2, 4, 3];
+let arr1 = [0, 0, 1, 2];
 console.log(isAllPossibilities(arr1));
