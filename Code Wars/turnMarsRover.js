bfb950b95ad84b5b4fc962c46e91f7d2bca2e1f8
John Cuevas <jpmcuevas20@gmail.com>
2022-06-26 16:00:04 -0700

    alternate solution for directing mars rover

--- Code Wars/turnMarsRover.js
+++ Code Wars/turnMarsRover.js
@@ -1,21 +1,33 @@
 // create function that tells mars rover whether to turn left or right based on current direction and target
 
 // my solution
-const turn = (current, target) =>{
-    if(current == 'N' && target == 'E'){
+const turn = (current, target) => {
+    if (current == 'N' && target == 'E') {
         return 'right';
     }
-    else if(current == 'E' && target == 'S'){
+    else if (current == 'E' && target == 'S') {
         return 'right';
     }
-    else if(current == "S" && target == 'W'){
+    else if (current == "S" && target == 'W') {
         return 'right';
     }
-    else if(current =='W' && target =='N'){
+    else if (current == 'W' && target == 'N') {
         return 'right';
     }
-    else{
+    else {
         return 'left';
     }
 
+}
+
+// Alternate solution
+const directions = {
+    N: { W: "left", E: "right" },
+    S: { W: "right", E: "left" },
+    W: { N: "right", S: "left" },
+    E: { N: "left", S: "right" },
+}
+
+function turn2(current, target) {
+    return directions[current][target]
 }
