19334498d6d7a46f76b4e5729aeb1d12c4679d43
John Cuevas <jpmcuevas20@gmail.com>
2022-06-27 14:28:32 -0700

    convert first letter to uppercase another notation

--- Code Wars/jadenCase.js
+++ Code Wars/jadenCase.js
@@ -1,13 +1,21 @@
 // convert first letter to uppercase
 // my solution
 
-const toJadenCase = (str) =>{
+const toJadenCase = (str) => {
     let words = str.split(' ');
-    for(let i =0; i < words.length; i++){
+    for (let i = 0; i < words.length; i++) {
         words[i] = (words[i].charAt(0).toUpperCase()) + words[i].slice(1);
     }
     return words.join(" ");
 }
 
+// another notation
+String.prototype.toJadenCase = function () {
+    let words = this.split(' ');
+    for (let i = 0; i < words.length; i++) {
+        words[i] = (words[i].charAt(0).toUpperCase()) + words[i].slice(1);
+    }
+    return words.join(" ");
+};
 let str1 = "How can mirrors be real if our eyes aren't real";
 console.log(toJadenCase(str1));
