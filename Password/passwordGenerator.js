96ee3d87099c0182ed7c85a1158d69a6a96efe93
John Cuevas <jpmcuevas20@gmail.com>
2022-07-05 13:26:00 -0700

    boolean check for password - up, low, num, pun

--- Password/passwordGenerator.js
+++ Password/passwordGenerator.js
@@ -1,12 +1,29 @@
-const passwordGenerator = (length)=>{
-    let password ="";
-    let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    
-    while(password.length < length){
-        password += possible.charAt(Math.floor(Math.random()* possible.length));
+const passwordGenerator = (length, low, up, num, pun) => {
+    let password = "";
+    let lowercase = "abcdefghijklmnopqrstuvwxyz";
+    let uppercase = lowercase.toUpperCase();
+    let numbers = "0123456789";
+    let punctuation = "!@#$%";
+    let char = [lowercase, uppercase, numbers, punctuation];
+    let possible = "";
+    let bool = [low, up, num, pun];
+
+    for (let i = 0; i < bool.length; i++) {
+        if (bool[i] == true) {
+            possible += char[i];
+        }
+    }
+
+    while (password.length < length) {
+        password += possible.charAt(Math.floor(Math.random() * possible.length));
     }
     return password;
+
 }
 
 let length = 8;
-console.log(passwordGenerator(length))
+let low = true;
+let up = false;
+let num = true;
+let pun = true;
+console.log(passwordGenerator(length, low, up, num, pun))
