dd6696256288264b9a801361d4e5f1f0ac4ebb6f
John Cuevas <jpmcuevas20@gmail.com>
2022-07-08 11:30:35 -0700

    valid parentheses closed "()"

---
+++ Amazon Leetcode/validParentheses.js
@@ -0,0 +1,28 @@
+const isValid = (s) =>{
+    let result =[];
+    for(let i = 0; i < s.length; i++){
+        let left = s[i];
+        let right = s[i+1];
+        if(left == "(" && right == ")"){
+            result.push(left, right);
+        }
+        else if(left == "[" && right == "]"){
+            result.push(left, right);
+        }
+        else if(left == "{" && right == "}"){
+            result.push(left, right);
+        }
+    }
+    if(result.length == s.length){
+        return true;
+    }
+    return false;
+}
+let s1 = "()"
+let s2 = "()[]{}"
+let s3 = "(]"
+let s4 = "()[{}"
+console.log(isValid(s1));
+console.log(isValid(s2));
+console.log(isValid(s3));
+console.log(isValid(s4));
