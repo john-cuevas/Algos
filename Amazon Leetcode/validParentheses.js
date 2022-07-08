2a4226e6b9311702c768a4a461edde1df0c70e22
John Cuevas <jpmcuevas20@gmail.com>
2022-07-08 15:28:27 -0700

    valid parentheses refactor code

--- Amazon Leetcode/validParentheses.js
+++ Amazon Leetcode/validParentheses.js
@@ -3,13 +3,7 @@ const isValid = (s) =>{
     for(let i = 0; i < s.length; i++){
         let left = s[i];
         let right = s[i+1];
-        if(left == "(" && right == ")"){
-            result.push(left, right);
-        }
-        else if(left == "[" && right == "]"){
-            result.push(left, right);
-        }
-        else if(left == "{" && right == "}"){
+        if((left == "(" && right == ")") || (left == "[" && right == "]") || (left == "{" && right == "}")){
             result.push(left, right);
         }
     }
@@ -22,7 +16,9 @@ let s1 = "()"
 let s2 = "()[]{}"
 let s3 = "(]"
 let s4 = "()[{}"
+let s5 = " "
 console.log(isValid(s1));
 console.log(isValid(s2));
 console.log(isValid(s3));
 console.log(isValid(s4));
+console.log(isValid(s5));
