3ccfab1e07842be48809118ba7ed958818a49a36
John Cuevas <jpmcuevas20@gmail.com>
2022-07-13 21:36:24 -0700

    sort colors

---
+++ Amazon Leetcode/sortColors.js
@@ -0,0 +1,14 @@
+// sort colors adjacent in the order of red, white, blue
+// 0 -> red
+// 1 -> white
+// 2 -> blue
+
+// solve without using sort function
+
+const sortColors = (nums) =>{
+    let result = [];
+    for(let i = 0; i < nums.length; i++){
+        
+    }
+
+}
