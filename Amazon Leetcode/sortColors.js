4af9e04cfe6df8785c7759fa568d8e3eedd60b7d
John Cuevas <jpmcuevas20@gmail.com>
2022-07-14 21:21:48 -0700

    sort colors

--- Amazon Leetcode/sortColors.js
+++ Amazon Leetcode/sortColors.js
@@ -5,10 +5,38 @@
 
 // solve without using sort function
 
-const sortColors = (nums) =>{
+const sortColors = (nums) => {
+    let count = 0;
+    let count1 = 0;
+    let count2 = 0;
     let result = [];
-    for(let i = 0; i < nums.length; i++){
-        
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] == 0) {
+            count++;
+        }
+        else if(nums[i] == 1){
+            count1++;
+        }
+        else if(nums[i] == 2){
+            count2++;
+        }
+    }
+    for(let i =0; i < nums.length; i++){
+        if(count > 0){
+            nums[i] = 0;
+            count--;
+        }
+        else if(count1 > 0){
+            nums[i] = 1;
+            count1--;
+        }
+        else {
+            nums[i] = 2;
+        }
     }
 
+    return nums;
+
 }
+let nums1 = [2, 0, 2, 1, 1, 0];
+console.log(sortColors(nums1))
