6294e03a899963267a3a5c73a4ca4b761c569dfd
John Cuevas <jpmcuevas20@gmail.com>
2022-07-19 16:12:12 -0700

    check valid ip

---
+++ Code Wars/isValidIP.js
@@ -0,0 +1,25 @@
+const isValidIP = str => {
+    const values = str.split(".")
+    console.log(values)
+    if (values.length < 4 || values.length < 0) {
+        return false;
+    }
+    for (let i = 0; i < values.length; i++) {
+        const value = values[i];
+        // contains character
+        const invalidCharacter = value.indexOf("e") > -1;
+        const negativeValue = value < 0;
+        // starts with leading 0
+        const invalidZero = value.length > 1 && value.indexOf("0") == 0;
+        const maxExceeded = value > 255;
+        // contains spaces
+        const containSpaces = value.indexOf(" ") > -1 || value.indexOf("\n") > -1;
+        if (invalidCharacter || negativeValue || invalidZero || maxExceeded || containSpaces) {
+            return false;
+        }
+    }
+    return true;
+}
+
+let str = "1.2.3.450";
+isValidIP(str)
