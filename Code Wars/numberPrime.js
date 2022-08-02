c8afdf74a6d6d936ff692c2f363ccabf94a06bf3
John Cuevas <jpmcuevas20@gmail.com>
2022-08-02 15:15:48 -0700

    check if number is prime

---
+++ Code Wars/numberPrime.js
@@ -0,0 +1,23 @@
+const numberPrime = num =>{
+
+    // numbers have to be greater than 0
+    if(num < 0){
+        return false;
+    }
+    let result = [];
+    for(let i = 0; i <= num; i++){
+        if(num / i %1 ==0){
+            result.push(i);
+        }
+    }
+
+    if(result.length == 2){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
+let num = 11;
+console.log(numberPrime(num))
