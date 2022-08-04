6ba479fcba442faca788e6d7b2440c71580d82e0
John Cuevas <jpmcuevas20@gmail.com>
2022-08-04 12:36:10 -0700

    longest palindrome amazon leetcode

---
+++ Amazon Leetcode/longestPalindrome.js
@@ -0,0 +1,32 @@
+const longestPalindrome = s => {
+    // base case
+    if (s.length < 1) {
+        return "";
+    }
+    let maxSubStart = 0;
+    let maxSubLength = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        const lengthCenteredAtChar = expandAroundCenter(s, i, i);
+        const lengthCenteredAtSpace = expandAroundCenter(s, i, i + 1);
+        const longestSubAtChar = Math.max(lengthCenteredAtChar, lengthCenteredAtSpace)
+        if (longestSubAtChar > maxSubLength) {
+            maxSubLength = longestSubAtChar;
+            maxSubStart = i - Math.floor((maxSubLength - 1) / 2);
+        }
+    }
+
+    return s.substr(maxSubStart, maxSubLength);
+
+}
+
+function expandAroundCenter(s, left, right) {
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+        left--;
+        right++;
+    }
+    return right - left - 1;
+}
+
+let s = "babad";
+console.log(longestPalindrome(s))
